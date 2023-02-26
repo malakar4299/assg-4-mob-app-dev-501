48404543e50efeb75342f544af8bc8e8d692f6b7
CoToYo <56789038+CoToYo@users.noreply.github.com>
2023-02-25 22:41:28 -0500

    animationfragment

--- Hangman/app/src/main/java/com/example/hangmanapp/AnimationViewModel.kt
+++ Hangman/app/src/main/java/com/example/hangmanapp/AnimationViewModel.kt
@@ -1,7 +1,102 @@
 package com.example.hangmanapp
 
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.res.Resources
+import android.widget.Toast
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.android.material.snackbar.Snackbar
+import java.util.*
+import kotlin.coroutines.coroutineContext
 
-class AnimationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class AnimationViewModel() : ViewModel() {
+
+    private lateinit var answerArray: Array<String>
+    // final answer for current round
+    private var answer = ""
+    // display of secret word
+    private var display = ""
+    // record correct letters that player has chosen
+    private var correctLetters = mutableListOf<String>()
+    // iamge index
+    private var image_idx : Int = 0
+
+    val getImageIdx : Int
+        get() = image_idx
+
+    fun updateAnswer(answerArray : Array<String>): String {
+
+        val idx = Random().nextInt(answerArray.size)
+        answer = answerArray[idx]
+
+        return answer
+    }
+
+    /**
+     * supposed to get the letter from keyboard fragment
+     */
+    fun inputLetter(input : String) : Boolean{
+        var letter = input
+
+        // if player choose a correct letter, put it into 'correctLetter'
+        // and refresh the display
+        if(letter in answer.lowercase()){
+            correctLetters.add(letter)
+            updateDisplay()
+
+        }
+        else{
+            // wrong guess
+
+            // Player fails
+            image_idx++
+
+            return false
+        }
+
+        // win this round
+        if(checkIfWin()){
+            updateAnswer()
+            updateDisplay()
+            return true
+        }
+
+    }
+
+
+
+
+    /**
+     * update new display of the secret word
+     */
+    fun updateDisplay() {
+        answer.forEach {
+                s -> display += (checkCorrection(s.toString()))
+        }
+    }
+
+    /**
+     * reveal all correctly guessed letter,
+     * cover the other letters
+     */
+    fun checkCorrection(s: String) : String {
+        return when (correctLetters.contains(s.lowercase())) {
+            true -> s
+            false -> "_"
+        }
+    }
+
+    /**
+     * Check if player win this round
+     */
+    fun checkIfWin() : Boolean {
+        answer.lowercase().forEach { a ->
+            if(!correctLetters.contains(a.toString())){
+                return false
+            }
+        }
+        return true
+    }
 }
--- Hangman/app/src/main/java/com/example/hangmanapp/animationFragment.kt
+++ Hangman/app/src/main/java/com/example/hangmanapp/animationFragment.kt
@@ -6,26 +6,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.hangmanapp.databinding.FragmentAnimationBinding
 
 class animationFragment : Fragment() {
-
+    private lateinit var binding: FragmentAnimationBinding
     companion object {
         fun newInstance() = animationFragment()
     }
 
-    private lateinit var viewModel: AnimationViewModel
+    private val animationViewModel: AnimationViewModel = ViewModelProvider(requireActivity()).get(AnimationViewModel::class.java)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_animation, container, false)
+        var view = inflater.inflate(R.layout.fragment_animation, container, false)
+        var secret_word_view : TextView = view.findViewById<TextView>(R.id.secret_word_view)
+
+        var hangman_image : ImageView = view.findViewById(R.id.hangman_image)
+
+        val image_idx = animationViewModel.getImageIdx
+
+        when (image_idx) {
+            1 -> hangman_image.setImageResource(R.drawable.hangman1)
+            2 -> hangman_image.setImageResource(R.drawable.hangman2)
+            3 -> hangman_image.setImageResource(R.drawable.hangman3)
+            4 -> hangman_image.setImageResource(R.drawable.hangman4)
+            5 -> hangman_image.setImageResource(R.drawable.hangman5)
+            6 -> hangman_image.setImageResource(R.drawable.hangman6)
+            7 -> hangman_image.setImageResource(R.drawable.hangman7)
+            8 -> hangman_image.setImageResource(R.drawable.hangman8)
+            9 -> hangman_image.setImageResource(R.drawable.hangman9)
+            10 -> {
+                hangman_image.setImageResource(R.drawable.hangman10)
+            }
+        }
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(AnimationViewModel::class.java)
-        // TODO: Use the ViewModel
+        animationViewModel = ViewModelProvider(this).get(AnimationViewModel::class.java)
+
+    }
+
+    fun guess() {
+
     }
 
 }
